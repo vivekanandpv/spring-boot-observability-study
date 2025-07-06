eb848e1f54e46b213d336648c8e1133a8bc1a8a8
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-07-06 07:10:31 +0530

    Initial commit

---
+++ src/main/java/in/athenaeum/springbootobservabilitystudy/SpringBootObservabilityStudyApplication.java
@@ -0,0 +1,13 @@
+package in.athenaeum.springbootobservabilitystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootObservabilityStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootObservabilityStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/athenaeum/springbootobservabilitystudy/SpringBootObservabilityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package in.athenaeum.springbootobservabilitystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootObservabilityStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
